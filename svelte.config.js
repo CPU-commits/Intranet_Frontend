957a398133730f940e73650af65e97f6b0f2e427
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-04-22 20:44:26 -0400

    [update] Alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,35 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
+import { resolve } from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess(),
+	],
 
 	kit: {
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
+		vite: {
+			server: {
+				watch: {
+					usePolling: true,
+				},
+			},
 
-export default config;
+			resolve: {
+				alias: {
+					$components: resolve('./src/components'),
+                    $lib: resolve('./src/lib'),
+                    $utils: resolve('./src/utils'),
+                    $stores: resolve('./src/stores'),
+                    $models: resolve('./src/models'),
+				},
+			},
+		},
+	},
+}
+
+export default config
