fcb575ded47a33850abacd31b359cee81faa526c
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-05-17 21:54:28 -0400

    [add, update] User component

---
+++ src/components/User/File.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import { variables } from '$lib/variables'
+
+	import type { UserFiles } from '$models/users/files.model'
+	import { addToast } from '$stores/toasts'
+	import API from '$utils/APIModule'
+	import downloadFileURL from '$utils/downloadFileURL'
+	import { formatDate } from '$utils/format'
+	import getIcon from '$utils/getIcon'
+
+	export let file: UserFiles
+	export let token: string
+	export let date: string
+
+	async function downloadFile() {
+		try {
+			const dataFetch = await API.fetchGetData(
+				`${variables.API_FILES}/api/files/get_file/${file._id.$oid}`,
+				true,
+				token,
+			)
+			const ext = file.filename.split('.')
+			downloadFileURL(dataFetch.body.token)
+		} catch (err) {
+			addToast({
+				message: err.message,
+				type: 'error',
+			})
+		}
+	}
+</script>
+
+<button on:click={downloadFile} title={file.title}>
+	<article class="File">
+		<div class="File__icon">
+			<i class={getIcon(file.type)} />
+		</div>
+		<div class="File__text">
+			<h3>{file.title}</h3>
+			<small>
+				{file.filename}
+				{formatDate(date ? date : file.date)}
+			</small>
+		</div>
+	</article>
+</button>
+
+<style lang="scss">
+	button {
+		border: none;
+		background: transparent;
+	}
+
+	button:hover h3 {
+		color: var(--color-main);
+	}
+
+	.File {
+		width: fit-content;
+		display: flex;
+		min-width: 300px;
+	}
+
+	.File__icon {
+		background-color: var(--color-main);
+		padding: 20px;
+		border-top-left-radius: 10px;
+		border-bottom-left-radius: 10px;
+		i {
+			color: white;
+			font-size: 1rem;
+		}
+	}
+
+	.File__text {
+		border: 2px solid var(--color-light);
+		border-left: none;
+		padding: 5px;
+		display: flex;
+		width: 100%;
+		flex-direction: column;
+		justify-content: flex-start;
+		h3 {
+			font-size: 1.1rem;
+			transition: all 0.4s ease;
+		}
+	}
+</style>
---
+++ src/components/User/User.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { UserTypes } from '$models/users/user_type.model'
+	import AIcon from '../HTML/AIcon.svelte'
+
+	export let url: string
+	export let user_type: string
+</script>
+
+<section class="User">
+	<section class="Menu">
+		<ul>
+			<li>
+				<AIcon
+					active={url === '/usuario'}
+					href={'/usuario'}
+					classItem={'fa-solid fa-address-card'}
+				/>
+			</li>
+			{#if user_type !== UserTypes.STUDENT && user_type !== UserTypes.ATTORNEY}
+				<li>
+					<AIcon
+						href={'/usuario/archivos'}
+						classItem={'fa-solid fa-cloud'}
+						active={url === '/usuario/archivos'}
+					/>
+				</li>
+			{/if}
+			{#if user_type === UserTypes.STUDENT || user_type === UserTypes.STUDENT_DIRECTIVE || user_type === UserTypes.ATTORNEY}
+				<li>
+					<AIcon
+						href={'/usuario/calificaciones'}
+						classItem={'fa-solid fa-ranking-star'}
+						active={url === '/usuario/calificaciones'}
+					/>
+				</li>
+			{/if}
+		</ul>
+	</section>
+	<section class="Content">
+		<slot name="title" />
+		<br />
+		<slot />
+	</section>
+</section>
+
+<style>
+	.User {
+		display: flex;
+		flex-direction: column;
+		margin: 20px;
+		box-sizing: border-box;
+	}
+
+	.Menu ul {
+		display: flex;
+		list-style: none;
+		gap: 50px;
+		width: 100%;
+		justify-content: center;
+		padding: 10px;
+		box-sizing: border-box;
+	}
+
+	.Content {
+		background-color: white;
+		padding: 20px;
+		box-shadow: var(--box-shadow);
+	}
+</style>
