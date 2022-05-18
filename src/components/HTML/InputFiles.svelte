38c965d3e7d257be4424e7c8b3d3386488d7f323
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-05-17 21:54:04 -0400

    [add] Components HTML

---
+++ src/components/HTML/InputFiles.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let files: any
+	export let id: string
+</script>
+
+<input bind:files type="file" {id} />
+
+<style lang="scss">
+	input {
+		width: 100%;
+		padding: 10px;
+		box-sizing: border-box;
+		border: none;
+		border-bottom: 3px var(--color-light) solid;
+		transition: all 0.4s ease-in-out;
+	}
+
+	input:focus {
+		border-bottom: 3px var(--color-main) solid;
+		outline: none;
+	}
+</style>
---
+++ src/components/HTML/VerticalTable.svelte
@@ -0,0 +1,19 @@
+<section class="Table">
+	<table>
+		<tbody class="Tbody TableVertical">
+			<slot />
+		</tbody>
+	</table>
+</section>
+
+<style>
+	.Table {
+		overflow-x: auto;
+		width: 100%;
+	}
+
+	table {
+		width: 100%;
+		border-collapse: collapse;
+	}
+</style>
