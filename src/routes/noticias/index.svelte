4710d39bd0586fd9758241d809f643692ab8e99a
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-05-17 21:56:18 -0400

    [init] News section

---
+++ src/routes/noticias/index.svelte
@@ -0,0 +1,23 @@
+<button>
+	<aside class="AddNews">
+		<i class="fa-solid fa-plus" />
+	</aside>
+</button>
+
+<style lang="scss">
+	.AddNews {
+		position: fixed;
+		bottom: 20px;
+		right: 20px;
+		background-color: var(--color-news);
+		height: 50px;
+		width: 50px;
+		border-radius: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		i {
+			color: var(--color-news--black);
+		}
+	}
+</style>
