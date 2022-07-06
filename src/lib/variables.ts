e1eac2757b84d5bc0b7b77edca8f8992af5b5ab0
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-07-06 02:31:24 -0400

    [update] Add APIs

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,6 +1,10 @@
 export const variables = {
     API: import.meta.env.VITE_API.toString(),
     API_FILES: import.meta.env.VITE_API_FILES.toString(),
+    API_NEWS: import.meta.env.VITE_API_NEWS.toString(),
+    API_NOTIFICATIONS: import.meta.env.VITE_API_NOTIFICATIONS.toString(),
+    API_CLASSROOM_READ: import.meta.env.VITE_API_CLASSROOM_READ.toString(),
+    API_CLASSROOM_WRITE: import.meta.env.VITE_API_CLASSROOM_WRITE.toString(),
     CLIENT: import.meta.env.VITE_CLIENT,
     NODE_ENV: import.meta.env.VITE_NODE_ENV,
     redis: {
