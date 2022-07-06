8e1921795a94083e94655a8614be6bf645bc52f3
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-07-06 02:29:38 -0400

    [update] Add other format functions

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,24 @@
 import moment from 'moment-with-locales-es6'
+import { arabToRoman } from 'roman-numbers'
 
-export function formatDate(date: string) {
+export function formatDate(date: string | Date) {
     return moment(date).locale('es').utc().format('LLL')
+}
+
+export function timeAgo(date: string | Date) {
+    return moment(date).locale('es').utc().fromNow()
+}
+
+export function isValidHttpUrl(string: string): boolean {
+    let url: URL
+    try {
+        url = new URL(string)
+    } catch (err) {
+        return false
+    }
+    return url.protocol === 'http:' || url.protocol === 'https:'
+}
+
+export function intToRoman(num: number) {
+    return arabToRoman(num)
 }
