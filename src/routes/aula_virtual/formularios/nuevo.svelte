91c3c76a3347d44d94c89b9ab198c725a2f45abd
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-07-27 16:43:36 -0400

    [add] Forms

--- src/routes/aula_virtual/formularios/nuevo.svelte
+++ src/routes/aula_virtual/formularios/nuevo.svelte
@@ -24,13 +24,13 @@
 	import type { ItemType } from '$models/classroom/item.model'
 	import { addToast } from '$stores/toasts'
 	import API from '$utils/APIModule'
-	import { intToRoman } from '$utils/format'
 	import { deltaQuillToHtml } from '$utils/quill'
+	import { validateForm } from '$utils/validators'
 	import type Quill from 'quill'
 
 	let form = {
 		title: 'Nuevo formulario',
-		type: '',
+		has_points: '',
 		items: [] as Array<ItemType>,
 	}
 	let checked: number = 0
@@ -43,12 +43,11 @@
 				...form.items,
 				{
 					title: '',
-					type: '',
+					points_type: '',
 					questions: [
 						{
 							question: '',
 							type: '',
-							alternatives: [],
 							answers: [],
 							correct: 0,
 							points: '0',
@@ -72,127 +71,10 @@
 		form.items = form.items
 	}
 
-	function validators() {
-		if (form.title.length === 0 || form.title.length > 100)
-			throw new Error('El titlo del formulario debe ser de máx. 100 cárac.')
-		if (form.type === '') throw new Error('Debe seleccionar un puntaje (preguntas)')
-		if (form.items.length === 0) throw new Error('Debe haber mín. 1 item')
-		form.items.forEach((item, i) => {
-			if (item.title.length === 0 || item.title.length > 100)
-				throw new Error(
-					`El titulo del item ${intToRoman(i + 1)} debe ser de máx. 100 cárac.`,
-				)
-			if (form.type === 'true' && item.type === '')
-				throw new Error(
-					`Debe seleccionar una asignación de puntaje en el item ${intToRoman(i + 1)}`,
-				)
-			if (
-				item.type === 'equal' &&
-				(Number(item.points) <= 0 || !item.points) &&
-				form.type === 'true'
-			)
-				throw new Error(
-					`Debe indicar un puntaje total a distribuir mayor a 0 en el item ${intToRoman(
-						i + 1,
-					)}`,
-				)
-			if (item.questions.length === 0)
-				throw new Error(`Debe haber mín. 1 pregunta en el item ${intToRoman(i + 1)}`)
-			item.questions.forEach((question, iQ) => {
-				if (question.type === '')
-					throw new Error(
-						`Debe seleccionar tipo de pregunta en la pregunta ${
-							iQ + 1
-						} del item ${intToRoman(i + 1)}`,
-					)
-				if (deltaQuillToHtml(question.question).length === 0)
-					throw new Error(
-						`Debe escribir una pregunta en la pregunta ${iQ + 1} del item ${intToRoman(
-							i + 1,
-						)}`,
-					)
-				if (
-					item.type === 'custom' &&
-					form.type === 'true' &&
-					Number(question.points) <= 0 &&
-					question.type !== 'alternatives'
-				)
-					throw new Error(
-						`El puntaje debe ser mayor a 0 en la pregunta ${
-							iQ + 1
-						} del item ${intToRoman(i + 1)}`,
-					)
-				if (
-					item.type === 'custom' &&
-					form.type === 'true' &&
-					!Number.isInteger(Number(question.points)) &&
-					question.type !== 'alternatives'
-				)
-					throw new Error(
-						`El puntaje debe ser entero en la pregunta ${iQ + 1} del item ${intToRoman(
-							i + 1,
-						)}`,
-					)
-				if (question.type.includes('alternatives') && question.alternatives.length <= 1)
-					throw new Error(
-						`Deben existir mín. 2 alternativas en la pregunta ${
-							iQ + 1
-						} del item ${intToRoman(i + 1)}`,
-					)
-				if (question.type === 'written') {
-					question.answers.forEach((answer, iA) => {
-						if (answer.length === 0 || answer.length > 100)
-							throw new Error(
-								`La respuesta ${
-									iA + 1
-								} debe ser de máx. 100 cárac. de la pregunta ${
-									iQ + 1
-								} del item ${intToRoman(i + 1)}`,
-							)
-					})
-				} else {
-					question.alternatives.forEach((alternative, iA) => {
-						if (
-							alternative.alternative.length === 0 ||
-							alternative.alternative.length > 100
-						)
-							throw new Error(
-								`La alternativa ${iA + 1} de la pregunta ${
-									iQ + 1
-								} del item ${intToRoman(i + 1)} debe ser de máx. 100 cárac.`,
-							)
-					})
-				}
-				if (
-					question.type === 'alternatives_correct' &&
-					question.correct >= question.alternatives.length
-				)
-					throw new Error(
-						`Debe existir una respuesta correcta en la pregunta ${
-							iQ + 1
-						} del item ${intToRoman(i + 1)}`,
-					)
-			})
-			if (item.type === 'equal' && form.type === 'true') {
-				const countQuestions = item.questions.reduce((a, b) => {
-					const count = b.type !== 'alternatives' ? 1 : 0
-					return a + count
-				}, 0)
-				const pointsPerQuestion = Number(item.points) / countQuestions
-				if (!Number.isInteger(pointsPerQuestion))
-					throw new Error(
-						`El puntaje equitativo para cada pregunta del item ${intToRoman(
-							i + 1,
-						)} no es entero. (Resulta ${pointsPerQuestion})`,
-					)
-			}
-		})
-	}
-
 	function buildData() {
 		return {
 			title: form.title,
-			type: form.type,
+			has_points: form.has_points,
 			items: form.items.map((item) => {
 				const itemData: any = {
 					title: item.title,
@@ -200,21 +82,17 @@
 						const questionData: any = {
 							question: deltaQuillToHtml(question.question),
 							type: question.type,
+							answers: question.answers,
 						}
-						if (question.type.includes('alternatives'))
-							questionData.alternatives = question.alternatives.map((alternative) => {
-								return alternative.alternative
-							})
-						if (question.type === 'written') questionData.answers = question.answers
 						if (questionData.type === 'alternatives_correct')
 							questionData.correct = question.correct
-						if (item.type === 'custom' && form.type === 'true')
+						if (item.points_type === 'custom' && form.has_points === 'true')
 							questionData.points = parseInt(question.points)
 						return questionData
 					}),
 				}
-				if (form.type === 'true') itemData.type = item.type
-				if (item.type === 'equal' && form.type === 'true')
+				if (form.has_points === 'true') itemData.points_type = item.points_type
+				if (item.points_type === 'equal' && form.has_points === 'true')
 					itemData.points = parseInt(item.points)
 				return itemData
 			}),
@@ -223,7 +101,7 @@
 
 	async function uploadForm() {
 		try {
-			validators()
+			validateForm(form)
 			const data = buildData()
 			await API.fetchData(
 				'post',
@@ -251,7 +129,7 @@
 	<input bind:value={form.title} type="text" placeholder="Titulo" />
 	<Form form={uploadForm}>
 		<label for="score">Puntaje (Preguntas)</label>
-		<Select bind:value={form.type} id={'score'}>
+		<Select bind:value={form.has_points} id={'score'}>
 			<option value="">Seleccionar tipo de puntaje</option>
 			<option value="true">Con puntaje</option>
 			<option value="false">Sin puntaje</option>
@@ -261,7 +139,7 @@
 			{#each form.items as item, i}
 				<Item
 					destroy={() => deleteItem(i)}
-					type={form.type}
+					type={form.has_points}
 					bind:question
 					bind:quill
 					bind:item
