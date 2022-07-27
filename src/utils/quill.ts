6a451ef480a725f19d3787db5490bf42c42c8876
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-07-27 16:42:01 -0400

    [update] Quill converts

--- src/utils/quill.ts
+++ src/utils/quill.ts
@@ -1,4 +1,5 @@
-import type Quill from 'quill'
+import Quill from 'quill'
+import type Delta from 'quill-delta'
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html'
 
 export function deltaToHtml(quill: Quill): string {
@@ -8,8 +9,21 @@ export function deltaToHtml(quill: Quill): string {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function deltaQuillToHtml(delta: any) {
+export function deltaQuillToHtml(delta: Delta) {
     const cfg = {}
     const converter = new QuillDeltaToHtmlConverter(delta.ops, cfg)
     return converter.convert()
+}
+
+export function htmlToDelta(html: string, document?: Document) {
+    const div = document.createElement('div')
+    div.setAttribute('id', 'htmlToDelta')
+    div.innerHTML = `<div id="quillEditor" style="display:none">${html}</div>`
+    document.body.appendChild(div)
+    const quill = new Quill('#quillEditor', {
+        theme: 'snow',
+    })
+    const delta = quill.getContents()
+    document.getElementById('htmlToDelta').remove()
+    return delta
 }
