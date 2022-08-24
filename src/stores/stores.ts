f3f2a79c14d7b8eb89855651d251ff3b16408427
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-08-24 05:42:11 -0400

    [add] Voting status store

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -1,3 +1,5 @@
+import type { VotingStatus } from "$models/voting.model"
 import { writable } from "svelte/store"
 
 export const spinner = writable(false)
+export const voting = writable<keyof typeof VotingStatus>('waiting')
