224451e931d939c786d4acab00438c3377955a01
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-08-24 05:57:06 -0400

    [add] New notification type

--- src/models/notifications/notification.type.ts
+++ src/models/notifications/notification.type.ts
@@ -1,13 +1,29 @@
-export type NotifyGlobal = {
+import type { Subject } from "$models/admin/subjects.model"
+
+enum NotificationTypes {
+    'global',
+    'classroom',
+}
+
+enum TypeClassroom {
+    'publication',
+    'work',
+    'grade',
+}
+
+export type Notification = {
     title: string
     url: string
-    img: string
+    img?: string
+    subject?: Subject
+    type_classroom: keyof typeof TypeClassroom
+    type: keyof typeof NotificationTypes
 }
 
 export type NotificationType = {
     _id: string
     date: string | Date
-    notification: NotifyGlobal
+    notification: Notification
 }
 
 export type NotificationsFetch = {
