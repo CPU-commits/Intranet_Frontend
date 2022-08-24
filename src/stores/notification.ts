0cc5ab2a19c1412cefa691bfb953bcbded29ca75
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-08-24 05:52:57 -0400

    [change] Dont store notifications in localStorage

--- src/stores/notification.ts
+++ src/stores/notification.ts
@@ -1,18 +1,5 @@
-import { browser } from "$app/env"
 import { writable } from "svelte/store"
 
-const defaultValue = 0
-const initialValue = browser
-    ? JSON.parse(window.localStorage.getItem('notification'))
-    ?? defaultValue
-    : defaultValue
-
-export const notificationNumber = writable<number>(initialValue)
-
-notificationNumber.subscribe((value: number) => {
-    if(browser) {
-        window.localStorage.setItem('notification', value.toString())
-    }
-})
+export const notificationNumber = writable<number>(0)
 
 export { notificationNumber as default }
