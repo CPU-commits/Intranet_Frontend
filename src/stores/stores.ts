02488829bd2c9cf3661b8f4208e3352910f6b604
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-10-01 22:01:49 -0300

    [update] Add boolean type

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -1,5 +1,5 @@
 import type { VotingStatus } from "$models/voting.model"
 import { writable } from "svelte/store"
 
-export const spinner = writable(false)
+export const spinner = writable<boolean>(false)
 export const voting = writable<keyof typeof VotingStatus>('waiting')
