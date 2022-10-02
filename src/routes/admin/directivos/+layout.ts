068f64acb8cb4d7cec129f739ef51a2800858255
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-10-01 21:56:45 -0300

    [fix] layout

--- src/routes/admin/directivos/+layout.ts
+++ src/routes/admin/directivos/+layout.ts
@@ -1,7 +1,9 @@
+import type { Session } from '$models/session.model'
 import { UserTypes } from '$models/users/user_type.model'
 import { error } from '@sveltejs/kit'
 
-export function load({ session }) {
+export async function load({ parent }) {
+    const session: Session = await parent()
 	if (session?.user?.user_type !== UserTypes.DIRECTOR)
 		throw error(401, 'No tienes acceso a esta ruta')
 	return {}
