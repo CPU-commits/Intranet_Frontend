e530d9aa7992fe733e6a5931091bbff3c0e25255
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-10-01 22:04:14 -0300

    [update] Add type

--- src/routes/biblioteca/autor/[author]/+page.server.ts
+++ src/routes/biblioteca/autor/[author]/+page.server.ts
@@ -1,4 +1,6 @@
 import { variables } from "$lib/variables"
+import type { FetchResponse } from "$models/HTTP/fetchDataResponse.model"
+import type { Author } from "$models/library/author.model"
 import type { Session } from "$models/session.model"
 import API from "$utils/APIModule"
 import { error } from "@sveltejs/kit"
@@ -7,7 +9,7 @@ import { error } from "@sveltejs/kit"
 export async function load({ parent, params }) {
     try {
         const session: Session = await parent()
-        const dataFetch = await API.fetchGetData(
+        const dataFetch: FetchResponse<{ author: Author }> = await API.fetchGetData(
             `${variables.API_LIBRARY}/api/authors/get_author/${params.author}`,
             false,
             session.user.token,
