c06b8ec9a5de7fab06db71495d4218641c98fc18
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-10-01 22:08:21 -0300

    [delete] Redis envs

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -8,10 +8,4 @@ export const variables = {
     API_LIBRARY: import.meta.env.VITE_API_LIBRARY.toString(),
     CLIENT: import.meta.env.VITE_CLIENT,
     NODE_ENV: import.meta.env.VITE_NODE_ENV,
-    redis: {
-        user: import.meta.env.VITE_REDIS_USER,
-        password: import.meta.env.VITE_REDIS_PASSWORD,
-        port: import.meta.env.VITE_REDIS_PORT,
-        host: import.meta.env.VITE_REDIS_HOST,
-    },
 }
